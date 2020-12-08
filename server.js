136f4061936718bb0a5a8d61344631426c011e6d
Sam Fryzek <spfryzek@gmail.com>
2020-12-08 17:23:52 -0500

    initial

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API is Running'));
+app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
 
