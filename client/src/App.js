681c1461adf3cab817384ac14a39c38ebf336659
Sam Fryzek <spfryzek@gmail.com>
2020-12-09 12:59:07 -0500

    Updated dependencies so store.js and reducers works

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,35 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
-const App = () => (
-    <Router>
-        <Fragment>
-            <Navbar />
-            <Route exact path="/">
-                <Landing></Landing>
-            </Route>
-            <section className="container">
-                <Switch>
-                    <Route exact path="/register">
-                        <Register></Register>
+const App = () => {
+    return (
+        <Provider store={store}>
+            <Router>
+                <Fragment>
+                    <Navbar />
+                    <Route exact path="/">
+                        <Landing></Landing>
                     </Route>
-                    <Route exact path="/login">
-                        <Login></Login>
-                    </Route>
-                </Switch>
-            </section>
-        </Fragment>
-    </Router>
-);
+                    <section className="container">
+                        <Switch>
+                            <Route exact path="/register">
+                                <Register></Register>
+                            </Route>
+                            <Route exact path="/login">
+                                <Login></Login>
+                            </Route>
+                        </Switch>
+                    </section>
+                </Fragment>
+            </Router>
+        </Provider>
+    );
+};
 
 export default App;
