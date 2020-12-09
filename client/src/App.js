c4a6768391671d5156578a1718c4ec6796904c0b
Sam Fryzek <spfryzek@gmail.com>
2020-12-09 07:30:44 -0500

    Started adding frontend stuff

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import './App.css';
+
+const App = () => (
+    <Router>
+        <Fragment>
+            <Navbar />
+            <Route exact path="/">
+                <Landing></Landing>
+            </Route>
+            <section className="container">
+                <Switch>
+                    <Route exact path="/register">
+                        <Register></Register>
+                    </Route>
+                    <Route exact path="/login">
+                        <Login></Login>
+                    </Route>
+                </Switch>
+            </section>
+        </Fragment>
+    </Router>
+);
+
+export default App;
